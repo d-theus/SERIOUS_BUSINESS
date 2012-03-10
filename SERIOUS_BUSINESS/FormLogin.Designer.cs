6f9f99315c94804e3e04bd098282ede0f3c3638e
unknown <Slowness@Slowness-pc.(none)>
2012-03-10 08:43:08 +0300

    GUI

---
+++ SERIOUS_BUSINESS/FormLogin.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace SERIOUS_BUSINESS
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_login = new System.Windows.Forms.TextBox();
+            this.cb_remember = new System.Windows.Forms.CheckBox();
+            this.btn_enter = new System.Windows.Forms.Button();
+            this.tb_passwd = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Добро пожаловать";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите логин и пароль чтобы войти";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Логин";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Пароль";
+            // 
+            // tb_login
+            // 
+            this.tb_login.Location = new System.Drawing.Point(18, 82);
+            this.tb_login.Name = "tb_login";
+            this.tb_login.Size = new System.Drawing.Size(100, 20);
+            this.tb_login.TabIndex = 5;
+            // 
+            // cb_remember
+            // 
+            this.cb_remember.AutoSize = true;
+            this.cb_remember.Location = new System.Drawing.Point(18, 157);
+            this.cb_remember.Name = "cb_remember";
+            this.cb_remember.Size = new System.Drawing.Size(111, 17);
+            this.cb_remember.TabIndex = 6;
+            this.cb_remember.Text = "Запомнить меня";
+            this.cb_remember.UseVisualStyleBackColor = true;
+            // 
+            // btn_enter
+            // 
+            this.btn_enter.Location = new System.Drawing.Point(163, 148);
+            this.btn_enter.Name = "btn_enter";
+            this.btn_enter.Size = new System.Drawing.Size(57, 23);
+            this.btn_enter.TabIndex = 7;
+            this.btn_enter.Text = "Войти";
+            this.btn_enter.UseVisualStyleBackColor = true;
+            this.btn_enter.Click += new System.EventHandler(this.btn_enter_Click);
+            // 
+            // tb_passwd
+            // 
+            this.tb_passwd.Location = new System.Drawing.Point(19, 125);
+            this.tb_passwd.Name = "tb_passwd";
+            this.tb_passwd.PasswordChar = '*';
+            this.tb_passwd.Size = new System.Drawing.Size(100, 20);
+            this.tb_passwd.TabIndex = 8;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 183);
+            this.Controls.Add(this.tb_passwd);
+            this.Controls.Add(this.btn_enter);
+            this.Controls.Add(this.cb_remember);
+            this.Controls.Add(this.tb_login);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormLogin";
+            this.Text = "PhotoAdm 0.001";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormLogin_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_login;
+        private System.Windows.Forms.CheckBox cb_remember;
+        private System.Windows.Forms.Button btn_enter;
+        private System.Windows.Forms.TextBox tb_passwd;
+    }
+}
+
---
+++ SERIOUS_BUSINESS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SERIOUS_BUSINESS
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            FormMain formMain = new FormMain();
+            formMain.Hide();
+            Application.Run(formMain);
+        }
+    }
+}
