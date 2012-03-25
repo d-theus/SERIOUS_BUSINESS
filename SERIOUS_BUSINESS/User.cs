ebfb3e8edbacb7e3471fb2a7e6d7b13b7531e10b
unknown <Slowness@Slowness-PC.(none)>
2012-03-25 10:41:22 +0400

    final model conception

---
+++ SERIOUS_BUSINESS/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SERIOUS_BUSINESS
+{
+    public class User
+    {
+        public string name;
+
+        public string passwd;
+
+        public User(string _name)
+        {
+            this.name = _name;
+        }
+    }
+}
