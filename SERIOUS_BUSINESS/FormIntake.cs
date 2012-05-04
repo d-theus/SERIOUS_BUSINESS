8d6142b478e93c27ce7aa8b77bb8c1e73205d133
slowness - home <Slowness@Slowness-PC.(none)>
2012-05-05 00:00:17 +0400

    All done with no concurrency handling

--- SERIOUS_BUSINESS/FormIntake.cs
+++ SERIOUS_BUSINESS/FormIntake.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -11,16 +12,90 @@ namespace SERIOUS_BUSINESS
 {
     public partial class FormIntake : Form
     {
+        private res.Model1Container database;
+        private IQueryable<res.ItemCategory> Categories;
+        private IQueryable<res.Item> CurrentCatItems;
+        private IQueryable<res.ItemParameter> CurrentCatDesignations;
+
         public FormIntake()
         {
             InitializeComponent();
+            database = new res.Model1Container();
+            database.Connection.Open();
+            cb_type.IsAccessible = false;
+            cb_designation.IsAccessible = false;
+            btn_accept.Enabled = false;
+
+            FillCategories();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void FillCategories()
         {
-            this.DialogResult = DialogResult.Cancel;
-            this.Close();
+            Categories = from cats in database.ItemCategorySet select cats;
+            cb_type.DataSource = Categories.ToArray<res.ItemCategory>();
+            cb_type.DisplayMember = "name";
+            cb_type.ValueMember = "id";
+            cb_type.IsAccessible = true;
+        }
+        private void RefillItems()
+        {
+            if (!cb_type.Text.Equals("") && cb_type.IsAccessible)
+            {
+                Int32 selected_cat_id = int.Parse(cb_type.SelectedValue.ToString());
+                CurrentCatItems = from items in database.ItemSet where items.catID == selected_cat_id select items;
+
+                int designation_parameter_id = (from PC in database.ParameterCategorySet where PC.name.Equals("Наименование") select PC.id).Sum();
+                CurrentCatDesignations = from param in database.ItemParameterSet
+                                             join item in database.ItemSet on param.itemID equals item.id
+                                             where item.catID == selected_cat_id && param.ParameterCategory.name.Equals("Наименование")
+                                             select param;
+                cb_designation.DataSource = CurrentCatDesignations;
+                cb_designation.DisplayMember = "valueTxt";
+                cb_designation.ValueMember = "itemID";
+                cb_designation.IsAccessible = true;
+            }
         }
 
+        private void cb_type_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefillItems();
+        }
+
+        private void num_count_ValueChanged(object sender, EventArgs e)
+        {
+            if (num_count.Value == 0)
+                btn_accept.Enabled = false;
+            else
+                btn_accept.Enabled = true;
+        }
+
+        private void btn_accept_Click(object sender, EventArgs e)
+        {
+            DialogResult appmnt = MessageBox.Show(string.Format("Действительно занести данные о поступлении {0} единиц товара '{1}'", (Int32)num_count.Value, (string)cb_designation.Text), "Внимание", MessageBoxButtons.YesNo);
+            if (appmnt == DialogResult.No) return;
+            int selected_id = -1;
+            selected_id = int.Parse(cb_designation.SelectedValue.ToString());
+            CurrentCatItems.Single<res.Item>(i => i.id == selected_id).storeResidue += (Int32)num_count.Value;
+            try_save_changes:
+            try
+            {
+                database.SaveChanges();
+            }
+            catch (Exception exc)
+            {
+                DialogResult reslt = MessageBox.Show(exc.Message, "Ошибка", MessageBoxButtons.RetryCancel);
+                switch (reslt)
+                {
+                    case DialogResult.Cancel:
+                        return;
+                    case DialogResult.Retry:
+                        goto try_save_changes;
+                }
+            }
+        }
+
+        private void FormIntake_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
     }
 }
